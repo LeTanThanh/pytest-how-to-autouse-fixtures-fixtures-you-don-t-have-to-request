d5b2a370692f1b7bc12f8a8a767b8f7fd58cb28f
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-06-24 18:22:20 +0700

    Add test_main.py

---
+++ test_main.py
@@ -0,0 +1,25 @@
+from pytest import fixture
+
+
+@fixture
+def first_entry():
+    return "a"
+
+
+@fixture
+def order(first_entry):
+    return []
+
+
+@fixture(autouse=True)
+def append_first(order, first_entry):
+    return order.append(first_entry)
+
+
+def test_string_only(order, first_entry):
+    assert order == [first_entry]
+
+
+def test_string_and_int(order, first_entry):
+    order.append(2)
+    assert order == [first_entry, 2]
